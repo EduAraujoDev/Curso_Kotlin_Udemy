4a67f65066f721615a182f4c732c6b48bbee1e3c
Eduardo Araújo <eduardo.araujo0@outlook.com>
2021-04-02 16:19:11 -0300

    App Tasks - Listagem de tarefas

--- TasksTemplate/app/src/main/java/com/example/tasks/service/repository/PriorityRepository.kt
+++ TasksTemplate/app/src/main/java/com/example/tasks/service/repository/PriorityRepository.kt
@@ -32,4 +32,6 @@ class PriorityRepository(context: Context) {
     }
 
     fun list() = mPriorityDatabase.list()
+
+    fun getDescription(id: Int) = mPriorityDatabase.getDescription(id)
 }
--- TasksTemplate/app/src/main/java/com/example/tasks/service/repository/TaskRepository.kt
+++ TasksTemplate/app/src/main/java/com/example/tasks/service/repository/TaskRepository.kt
@@ -16,6 +16,21 @@ class TaskRepository(val context: Context) {
 
     private val mRemote = RetrofitClient.createService(TaskService::class.java)
 
+    fun all(listener: APIListener<List<TaskModel>>) {
+        val call: Call<List<TaskModel>>  = mRemote.all()
+        list(call, listener)
+    }
+
+    fun nextWeek(listener: APIListener<List<TaskModel>>) {
+        val call: Call<List<TaskModel>>  = mRemote.nextWeek()
+        list(call, listener)
+    }
+
+    fun overduo(listener: APIListener<List<TaskModel>>) {
+        val call: Call<List<TaskModel>>  = mRemote.expired()
+        list(call, listener)
+    }
+
     fun create(task: TaskModel, listener: APIListener<Boolean>) {
         val call: Call<Boolean> =
             mRemote.create(task.priorityId, task.description, task.dueDate, task.complete)
@@ -30,6 +45,23 @@ class TaskRepository(val context: Context) {
             }
 
             override fun onFailure(call: Call<Boolean>, t: Throwable) {
+                listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED))
+            }
+        })
+    }
+
+    private fun list(call: Call<List<TaskModel>>, listener: APIListener<List<TaskModel>>) {
+        call.enqueue(object : Callback<List<TaskModel>>{
+            override fun onResponse(call: Call<List<TaskModel>>, response: Response<List<TaskModel>>) {
+                if (response.code() != TaskConstants.HTTP.SUCCESS) {
+                    val validation = Gson().fromJson(response.errorBody()!!.string(), String::class.java)
+                    listener.onFailure(validation)
+                } else {
+                    response.body()?.let { listener.onSuccess(it) }
+                }
+            }
+
+            override fun onFailure(call: Call<List<TaskModel>>, t: Throwable) {
                 listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED))
             }
         })
